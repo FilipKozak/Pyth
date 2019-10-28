076a4dab2ff83a778bdf345a0419611502501757
FilipKozak <57020177+FilipKozak@users.noreply.github.com>
2019-10-28 18:53:25 +0100

    Add files via upload

---
+++ Szcepan Generator.py
@@ -0,0 +1,17 @@
+﻿import random
+
+l = []
+
+print(" Witaj w generatorze SZCZEPANIAKA \n", "Postępuj zgodnie z poleceniami i wylosuj wybrańca\n", "Z każdym zapytaniem programu podaj tylko jednego uczestnika\n")
+while True:
+    uczestnicy = str(input('Podaj imiona uczestników. Jeżeli dodałeś już wszystkich wpisz end: '))
+    if 'end' in uczestnicy:
+        break
+    else:
+        print("Udział biorą: "),l.append(uczestnicy)
+        print(l)
+
+uczestnik = random.choice(l)
+print("Dla Szczepana prezkę robi: \n",uczestnik, "\n", "Gratulacje dla chętnego!!!\n", "Powodzenia :D\n")
+
+input ("Wcisnij Enter aby wyjść :)")
